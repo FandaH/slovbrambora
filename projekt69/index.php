aefa619ca5143b7e647310959d83d3168805ef0a
FandaH <49090206+FandaH@users.noreply.github.com>
2021-01-12 16:46:08 +0100

    Add files via upload

---
+++ projekt69/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+       <?php
+       
+       
+         echo 'Ahoj světe!<br /><br />';
+        $kokoska=true; 
+        
+$bourak="František Herink"; 
+$cislo=25; 
+$cena=48.2;
+$ahm = $bourak . $cena;
+
+
+
+
+
+
+
+
+    echo $kokoska;
+echo '<br /><br />';
+echo $bourak;
+echo '<br /><br />';
+echo $cislo;
+echo '<br /><br />';
+echo $cena;
+echo '<br /><br />';
+echo '<strong>odstavec34</strong>';
+    echo '<br /><br />';
+
+    print 'Ahoj světe!';
+print $kokoska;
+print '<br /><br />';
+print $bourak;
+print'<br /><br />';
+print $cislo;
+print '<br /><br />';
+print $cena;
+print '<br /><br />';
+    echo '<img src="kokos.jpg" alt"kokos">';
+
+    print '<br /><br />';
+echo '<strong>\\</strong>';
+    print '<br /><br />';
+
+
+    
+    
+    
+    
+    
+    
+    
+    $b = 25;
+    $a = 69;
+    
+    var_dump ($a+$b);
+var_dump ($a-$b);
+var_dump ($a/$b);
+var_dump ($a*$b);
+var_dump ($a++);
+var_dump ($a--);
+var_dump ($a+=$b);
+var_dump ($a-=$b);
+var_dump ($a*=$b);
+var_dump ($a/=$b);
+var_dump ($a==$b);
+var_dump ($a!=$b);
+var_dump ($a>$b);
+var_dump ($a<$b);
+var_dump ($a||$b);
+var_dump ($a&&$b);
+    var_dump (!$a);
+
+       
+
+        ?>
+    </body>
+</html>
